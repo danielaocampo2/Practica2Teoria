9b2c39a692488fdba061bbb200cc27d7557a8b5d
Simón Rodríguez <47600918+simonrodrigueza@users.noreply.github.com>
2020-05-09 17:54:17 -0500

    Ingreso hilera sin terminar

--- src/practica2teoria/Conjuntos.java
+++ src/practica2teoria/Conjuntos.java
@@ -5,11 +5,21 @@
  */
 package practica2teoria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author SIMON
  */
 public class Conjuntos {
     
+   private ArrayList<String> Nanulable = new ArrayList<>();
+   private ArrayList<String> Panulable = new ArrayList<>();
+   private List<List<String>> Nprimeros = new ArrayList<List<String>>();
+   private List<List<String>> Pprimeros = new ArrayList<List<String>>();
+   private List<List<String>> siguientes = new ArrayList<List<String>>();
+   private List<List<String>> seleccion = new ArrayList<List<String>>();
+   
     
 }
--- src/practica2teoria/FXMLDocumentController.java
+++ src/practica2teoria/FXMLDocumentController.java
@@ -8,37 +8,131 @@ package practica2teoria;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import static javafx.scene.input.KeyCode.BACK_SPACE;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
  * @author SIMON
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
+    private TextField NTProduccion;
     @FXML
     private TextField hileraProduccion;
+    @FXML
+    private Button btnAceptarNTP;
+    @FXML
+    private Button botonAceptarNTT;
+    @FXML
+    private Button botonBorrar;
+    @FXML
+    private Button botonAbre;
+    @FXML
+    private Button botonCierra;
+    @FXML
+    private Button btnBorrarUlt;
+
+    @FXML
+    public void NTProduccionHilera(javafx.scene.input.KeyEvent keyEvent) {
+        char car = keyEvent.getCharacter().charAt(0);
+        if (car < 'A' || car > 'Z') {
+            keyEvent.consume();
+        }
+
+        NTProduccion.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.BACK_SPACE || event.getCode() == KeyCode.DELETE) {
+                    event.consume(); // to cancel character-removing keys               
+                }
+            }
+        });
+        if (NTProduccion.getText().length() == 0) {
+            btnAceptarNTP.setDisable(true);
+        }
+        btnAceptarNTP.setDisable(false);
+        botonBorrar.setDisable(false);
+
+    }
+
+    @FXML
+    public void hileraProd(javafx.scene.input.KeyEvent keyEvent) {
+        char car = keyEvent.getCharacter().charAt(0);
+        if (((car < 'a' || car > 'z') && (car < 'A' || car > 'Z'))) {
+            keyEvent.consume();
+        }
+        if (!hileraProduccion.getText().isEmpty()) {
+            if (hileraProduccion.getText().charAt(hileraProduccion.getText().length() - 1) == '<') {
+                botonCierra.setDisable(false);
+                if (car < 'A' || car > 'Z') {
+                    keyEvent.consume();
+                }
+
+            }
+
+        }
+
+    }
+
+    @FXML
+    public void botonBorrarNTP(ActionEvent event) {
+        NTProduccion.setText("");
+        btnAceptarNTP.setDisable(true);
+    }
+
+    @FXML
+    public void botonAceptarNTP(ActionEvent event) {
+        NTProduccion.setDisable(true);
+        botonBorrar.setDisable(true);
+        btnAceptarNTP.setDisable(true);
+        hileraProduccion.setDisable(false);
+        botonAbre.setDisable(false);
+    }
+
+    @FXML
+    public void botonAbreAcc(ActionEvent event) {
+        hileraProduccion.setText(hileraProduccion.getText() + "<");
+        botonAbre.setDisable(true);
+        btnBorrarUlt.setDisable(true);
+    }
+
+    @FXML
+    public void botonCierraAcc(ActionEvent event) {
+        hileraProduccion.setText(hileraProduccion.getText() + ">");
+        botonCierra.setDisable(true);
+        botonAbre.setDisable(false);
+        btnBorrarUlt.setDisable(false);
+    }
     
     @FXML
-    private void botonSiguienteAccionado(ActionEvent event) {
-       String hilera = hileraProduccion.getText();
-        
+    public void btnBorrarUltAcc(ActionEvent event){
+        String hilera = hileraProduccion.getText();
+        String subHilera = "";
+        char ultCar = hilera.charAt(hilera.length()-1);
+        if(ultCar=='>'){
+            //FALTA TERMINAR ESTO
+
+            
+        }
         
     }
-    public void hileraProduccion(javafx.scene.input.KeyEvent keyEvent) {
-        
-    
+    public void recorreNT(String hilera){
+        String hileraAux="";
+        //char car = hilera.charAt(hilera.length())
     }
-    
+            
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+    }
+
 }
